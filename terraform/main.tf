d8d2dc15a956bd4e65ef79dd809be1c7f9af5170
ajay47k <ajay.senger2@gmail.com>
2026-04-28 07:08:55 -0700

    Fixed the main.tf

--- terraform/main.tf
+++ terraform/main.tf
@@ -1,29 +1,36 @@
-terraform/main.tf
 terraform {
   required_providers {
     aws = {
-source  = "hashicorp/aws"
-      version = "~> 4.0"
+      source  = "hashicorp/aws"
+      version = "~> 5.0"
     }
   }
 }
+
 provider "aws" {
-  region = "us-east-1"
+  region = "us-east-2"
 }
+
 resource "aws_apprunner_service" "staging" {
   service_name = "simple-flask-app-staging"
+
   source_configuration {
     image_repository {
-      image_identifier      = var.ecr_image_identifier
+      image_identifier      = var.image_uri
       image_repository_type = "ECR"
+      image_configuration {
+        port = "5000"
+      }
     }
     auto_deployments_enabled = false
   }
 }
-variable "ecr_image_identifier" {
+
+variable "image_uri" {
   description = "The full URI of the Docker image in ECR."
   type        = string
 }
+
 output "service_url" {
   value = aws_apprunner_service.staging.service_url
 }
